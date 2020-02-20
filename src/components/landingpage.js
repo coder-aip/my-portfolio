965e3540fe6706c7a18595cfbbb103716e58bf90
Alexandre Ip <alex20xx_zala@hotmail.com>
2020-02-19 20:03:52 -0600

    Update the landing page url

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -20,12 +20,12 @@ class Landing extends Component {
 
                             <div className="social-links">
                                 {/* LindenIn*/}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="https://www.linkedin.com/in/alexandre-ip/" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-linkedin-square" aria-hidden="true" />
                                 </a>
 
                                 {/* Github*/}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="https://github.com/coder-aip" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-github-square" aria-hidden="true" />
                                 </a>
                             </div>
