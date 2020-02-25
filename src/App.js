1e10b2ed8ca582484b9d45a11ccc60f098e1071d
Alexandre Ip <alex20xx_zala@hotmail.com>
2020-02-25 17:44:38 -0600

    Adding stuff to resume page (#4)
    Co-authored-by: wgv-aip <32580979+wgv-aip@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return(      
 <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="Title" scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/my-portfolio">MyPortfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -17,7 +17,7 @@ class App extends Component {
                 <Link to="/contact">Contacts</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/my-portfolio">MyPortfolio</Link>}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
